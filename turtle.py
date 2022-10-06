e2779fb5375e358c49be9242e55799cd3bc8531e
lyn12111 <112796070+lyn12111@users.noreply.github.com>
2022-10-06 13:37:03 +0900

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,21 @@
+print("환영합니다")
+print("파이썬의 세계에 오신 것을 환영합니다.")
+print("파이썬은 강력합니다.")
+
+print("반갑습니다. 파이썬!")
+print(2*3/10)
+print("Hello","World","!!!")
+
+print(7*24)
+
+import turtle
+turtle.shape('turtle')
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
